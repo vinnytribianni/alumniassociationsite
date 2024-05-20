1e5ea88300c86e663c27e17612ad910711274cda
vinnytribianni <vinnytribianni@gmail.com>
2024-05-20 01:15:52 -0400

    mobile friendly

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Dropdown, DropdownButton } from 'react-bootstrap';
 import { useNavigate, Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import Badge from '../Images/Badge.png';
 import '../CSS/Navbar.css';
 
@@ -10,24 +12,24 @@ const Navbar = () => {
   const nav = (id, parentTitle) => {
     navigate(`/${parentTitle}`, { state: { id } });
     window.scrollTo(0, 0);
-
   };
 
   const scrollToTop = () => {
     window.scrollTo(0, 0); // Scroll to the top function for the logo
   };
 
-
   return (
     <div className="banner">
-      <div>
+      <input type="checkbox" id="menu-toggle" className="menu-toggle" />
+      <label htmlFor="menu-toggle" className="sidebar-toggle"><FontAwesomeIcon icon={faBars} /></label>
+      <div className='option'>
         <DropdownButton id="dropdown-history-button" title="History" className="dropdown">
-          <Dropdown.Item onClick={() => nav('1', 'history')}>Our History</Dropdown.Item> 
-          <Dropdown.Item onClick={() => nav('2', 'history')}>RhoBoat</Dropdown.Item> 
-          <Dropdown.Item onClick={() => nav('3', 'history')}>Lost Alumni</Dropdown.Item> 
-        </DropdownButton> 
+          <Dropdown.Item onClick={() => nav('1', 'history')}>Our History</Dropdown.Item>
+          <Dropdown.Item onClick={() => nav('2', 'history')}>RhoBoat</Dropdown.Item>
+          <Dropdown.Item onClick={() => nav('3', 'history')}>Lost Alumni</Dropdown.Item>
+        </DropdownButton>
       </div>
-      <div>
+      <div className='option'>
         <DropdownButton id="dropdown-leadership-button" title="Leadership" className="dropdown">
           <Dropdown.Item onClick={() => nav('1', 'leadership')}>Officers & Trustees</Dropdown.Item>
           <Dropdown.Item onClick={() => nav('2', 'leadership')}>Lodge Management</Dropdown.Item>
@@ -40,13 +42,13 @@ const Navbar = () => {
           <div>Alpha Rho<br />Alumni Association</div>
         </Link>
       </div>
-      <div>
+      <div className='option'>
         <DropdownButton id="dropdown-events-button" title="Events" className="dropdown">
           <Dropdown.Item onClick={() => nav('1', 'events')}>Annual Dart Tournament</Dropdown.Item>
           <Dropdown.Item onClick={() => nav('2', 'events')}>Annual Golf Tournament</Dropdown.Item>
         </DropdownButton>
       </div>
-      <div>
+      <div className='option'>
         <DropdownButton id="dropdown-support-button" title="Support" className="dropdown">
           <Dropdown.Item onClick={() => nav('1', 'support')}>Donate</Dropdown.Item>
           <Dropdown.Item onClick={() => nav('2', 'support')}>Member Dues</Dropdown.Item>
@@ -57,8 +59,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-
-
-
-
